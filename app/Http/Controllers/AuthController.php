ae597fa80d4bf7c66aca6b0ac33878e112bfdccd
Elyradin <elyra.dina.oktaviani@gmail.com>
2026-02-04 20:52:36 +0700

    update Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,12 +12,12 @@ class AuthController extends Controller
 {
     public function showRegisterForm()
     {
-        return view('auth.register');
+        return view('Auth.register');
     }
 
     public function showLoginForm()
     {
-        return view('auth.login');
+        return view('Auth.login');
     }
 
     // API: Register
