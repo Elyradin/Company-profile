34595740402bacd821f9f39e849d7b489f795e94
Elyradin <elyra.dina.oktaviani@gmail.com>
2026-02-05 14:00:01 +0700

    update web

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login', function () { return view('auth.login'); })->name('login');
 Route::get('/register', function () { return view('auth.register'); })->name('register');
 
-// Halaman Dashboard & Admin (Hanya Tampilan)
-// Kita tidak pakai middleware 'auth' bawaan Laravel session, 
-// karena pengecekan login akan dilakukan oleh JavaScript di browser.
 Route::get('/dashboard', function () { return view('profile.index'); })->name('dashboard');
 Route::get('/services', function () { return view('user.services'); })->name('user.services');
 
